fb26244aba234df560d884edb81100b0fadc0b8c
nathaniacalista01 <nathania.calista01@gmail.com>
2024-02-16 10:58:51 +0700

    feat : basaic FE

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
-import { useState } from 'react'
-import './App.css'
-import { Box } from '@chakra-ui/react'
+import { useState } from "react";
+import "./App.css";
+import {
+  Box,
+  Button,
+  ButtonGroup,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Select,
+} from "@chakra-ui/react";
 
 function App() {
   // const [count, setCount] = useState(0)
@@ -13,9 +22,51 @@ function App() {
       display={"flex"}
       flexDir={"column"}
     >
-      Welcome to Cipher
+      <Heading>Welcome to Cipher</Heading>
+      <Box
+        display={"flex"}
+        flexDir={"column"}
+        alignSelf={"center"}
+        mt={"4rem"}
+        w={"50vw"}
+        gap={4}
+      >
+        <FormControl>
+          <FormLabel>Input Type</FormLabel>
+          <Select placeholder="Select your input type" size={"lg"}>
+            <option value="text">Text</option>
+            <option value="file">File</option>
+          </Select>
+        </FormControl>
+
+        <FormControl id="input-text">
+          <FormLabel>Input text:</FormLabel>
+          <Input placeholder="(plain)" />
+        </FormControl>
+        <FormControl>
+          <FormLabel>Algorithm :</FormLabel>
+          <Select placeholder="Select your encryption algorithm">
+            <option value="vignere"> Vignere Cipher Standard</option>
+            <option value="varian-vignere">
+              Varian Vignere Cipher Standard
+            </option>
+            <option value="extended-vignere">Extended Vignere</option>
+            <option value="playfair-cipher">Playfair Cipher</option>
+            <option value="affine-cipher">Affine Cipher</option>
+            <option value="hill-cipher">Hill Cipher</option>
+          </Select>
+        </FormControl>
+        <FormControl>
+          <FormLabel>Key :</FormLabel>
+          <Input type="text" placeholder="Enter your key" />
+        </FormControl>
+        <ButtonGroup variant="outline" spacing="6" mt={12}>
+          <Button colorScheme="blue">Ecnrypt</Button>
+          <Button>Decrypt</Button>
+        </ButtonGroup>
+      </Box>
     </Box>
-  )
+  );
 }
 
-export default App
+export default App;
